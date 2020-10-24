5b7eb41c0a93fe4bddb35ad46943ec2e74711849
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-24 14:25:40 +1100

    authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -14,9 +14,7 @@ const protect = asyncHandler(async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await (await User.findById(decoded.id)).isSelected(
-        '-password'
-      );
+      req.user = await User.findById(decoded.id).select('-password');
 
       next();
     } catch (error) {
@@ -32,4 +30,13 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-export { protect };
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('Not authorized as an admin');
+  }
+};
+
+export { protect, admin };
