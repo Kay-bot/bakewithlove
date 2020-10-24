bc4ceb3a8f1a9cb65629d657aed340f23a61d2d6
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-24 13:22:41 +1100

    user login

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,30 @@ import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
-import productRoute from './routes/productRoute.js';
+// routes
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
+// db
 connectDB();
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.use('/api/products', productRoute);
+// routes middleware
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
+// port
 const PORT = process.env.PORT || 5000;
-
 app.listen(
   PORT,
   console.log(
