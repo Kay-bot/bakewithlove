6d91517714d0a56785ba1bd3184e3f160354673e
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-26 13:16:24 +1100

    typo

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (userExists) {
     res.status(400);
-    throw new Error('User already exits');
+    throw new Error('User already exists');
   }
 
   const user = await User.create({
@@ -91,5 +91,4 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-
 export { authUser, getUserProfile, registerUser, updateUserProfile };
