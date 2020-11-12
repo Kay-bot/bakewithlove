8f26a186a8593652e0d7d2db7b14a375f54cb69f
KAY S <KS.KWANRUAN@GMAIL.com>
2020-11-12 21:11:10 +1100

    bug fixed

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -49,7 +49,7 @@ export const productDetailsReducer = (
 ) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
-      return { loading: true, product: [] };
+      return { ...state, loading: true };
     case PRODUCT_DETAILS_SUCCESS:
       return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAIL:
@@ -111,7 +111,7 @@ export const productReviewCreateReducer = (state = {}, action) => {
     case PRODUCT_CREATE_REVIEW_FAIL:
       return { loading: false, error: action.payload };
     case PRODUCT_CREATE_REVIEW_RESET:
-      return { product: {} };
+      return {};
     default:
       return state;
   }
